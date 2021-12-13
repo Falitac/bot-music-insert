61572170f259b31fc4f76ea73bef40db08655480
Falitac <fastkonrad@gmail.com>
2021-12-13 14:57:22 +0100

    multiple file input feature

--- sodin.py
+++ sodin.py
@@ -1,14 +1,23 @@
 import pyautogui, sys, time
 
 
-def loadSongsFromFile(song_list):
-	with open('songs.txt', 'r') as file:
-		for line in file:
-			song_list.append(line.strip())
+def loadSongsFromFile(song_list, file_name):
+	try:
+		with open(file_name, 'r') as file:
+			for line in file:
+				song_list.append(line.strip())
+	except FileNotFoundError:
+		print(f'Skipping {file_name}')
+
 
 def main():
+	files = ["songs.txt"]
+	if 1 < len(sys.argv):
+		files = sys.argv[1:]
+
 	song_list = []
-	loadSongsFromFile(song_list)
+	for file in files:
+		loadSongsFromFile(song_list, file)
 
 	print(f'Prepare for trouble! Songs to add: {len(song_list)}')
 	time.sleep(2);
@@ -16,7 +25,8 @@ def main():
 	for entry in song_list:
 		pyautogui.typewrite('-p ' + entry);
 		pyautogui.press('enter')
-		time.sleep(0.5);
+		time.sleep(0.4);
+	
 
 if __name__ == '__main__':
 	main()
